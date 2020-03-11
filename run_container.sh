a601dd0f99225acd2fe05743c0a26dcca7957f38
Swarchal <scott.warchal@gmail.com>
2020-03-11 17:00:42 +0100

    remove leftovers

--- run_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo docker run \
-    -v /home/swl_sur/allen_fnet_test_data:/root/projects/data \
-    bayesiallen \
-        --train_csv /root/projects/data/sample_data.csv \
-        --image_dir /root/projects/data \
-        --batch_size 2 \
-        --output_path /root/projects/data \
--- tests/test_package_import.py
+++
@@ -1,6 +0,0 @@
-from bayesiallen.fnet_nn_2d import Net as FNet
-
-
-def test_load():
-    model = FNet()
-    print(model)
